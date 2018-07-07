8197f2d56851f0ed00f6664d5bf79301f10be877
NebrasTaleb <MuhammadNebras.Taleb@ogr.iu.edu.tr>
2018-07-07 14:54:05 +0300

    Splash screen _ only displaying the layout.

--- app/src/main/java/com/safaorhan/reunion/activity/SplashActivity.java
+++ app/src/main/java/com/safaorhan/reunion/activity/SplashActivity.java
@@ -8,16 +8,26 @@ import android.support.v7.app.AppCompatActivity;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreSettings;
+import com.safaorhan.reunion.FirestoreHelper;
 import com.safaorhan.reunion.R;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 2500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                    SplashActivity.this.startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                    finish();
 
-        configureFirestore();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+        //configureFirestore();
     }
 
     private void configureFirestore() {
